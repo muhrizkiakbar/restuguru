3a5ea2c9acb94e7c873b9c0be2600d054bcb1a83
edofh <edofhmenos@gmail.com>
2018-07-21 23:06:43 +0800

    function log on controller and adding table activity_log

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\CActivityLog;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +14,44 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function createlog($log)
+    {
+        $table = new CActivityLog;
+        $table->log = $log;
+
+        if ($table->save()){
+            return response()->json("Success");
+        }else{
+            return response()->json("Failed");
+        }
+    }
+    public function updatelog($id, $log)
+    {
+        $table = CActivityLog::where('id','=',$id)
+                        ->first();
+        $table->log = $log;
+
+        if ($table->save()){
+            return response()->json("Success");
+        }else{
+            return response()->json("Failed");
+        }
+    }
+    public function deletelog($id)
+    {
+        $table = CActivityLog::where('id','=',$id)
+                        ->first();
+        if ($table->delete()){
+            return response()->json("Success");
+        }else{
+            return response()->json("Failed");
+        }
+    }
+    public function login(Request $request)
+    {
+    }
+    public function logout(Request $request)
+    {
+    }
 }
