cb56d3e5e065ea8e8685a235266401877a4bea24
Diskominfo <diskominfo@MacBook-Pro.local>
2019-03-19 19:45:57 +0800

    tes

--- app/Http/Controllers/AngsuranPenjualanController.php
+++ app/Http/Controllers/AngsuranPenjualanController.php
@@ -693,9 +693,9 @@ class AngsuranPenjualanController extends Controller
     public function reportangsuran($id)
     {
         $id=decrypt($id);
-        $data=Angsuran::where('transaksipenjualan_id','=',$id)->withTrashed()->get();
+        $data=Angsuran::where('transaksipenjualan_id','=',$id)->get();
         // dd('sdsd');
-        dd($data.' '.$id);
+        // dd($data.' '.$id);
         // dd($id);
         $transaksi=CTransaksi_Penjualans::leftJoin('Cabangs','Transaksi_Penjualans.cabang_id','=','Cabangs.id')
                     ->leftJoin('Users','Transaksi_Penjualans.user_id','=','Users.id')
