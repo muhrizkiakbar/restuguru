33f4d27c252cdbba8c6b644fa371ac09bbefa07b
Akbar <muhrizkiakbar@live.com>
2024-09-15 14:59:02 +0800

    a

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,5 @@
-
 #!/bin/bash
 set -e
 
-# Start PHP-FPM
-php-fpm &
-
-# Start Nginx
-nginx &
-
-# Start Supervisor to manage services
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+# Start Supervisor
+exec supervisord -c /etc/supervisord.conf
